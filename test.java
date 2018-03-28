2185866dec0dc9f901aa6a7741146790ab910ae3
Xiong Disheng <34514832+Roadscholar@users.noreply.github.com>
2018-03-28 17:08:58 +0800

    同步到new master
    在第一个分支上进行修改

--- test.java
+++ test.java
@@ -1,8 +1,9 @@
-public class test
+﻿public class test
 {
 	public static void main(String[] args)
 	{
 		System.out.println("H");
 		System.out.println("AAAA");
+		System.out.println("创建的第一个分支");
 	}
 }
